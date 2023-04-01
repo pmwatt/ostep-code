bad4367a197c1881cf1f9def4074b687f4fd03b4
Prachnachai Meakpaiboonwattana <prachnachai.m@gmail.com>
2023-04-01 17:23:29 +0700

    added console arg checker and w/r threads creation

--- muict-os-proj1-readerwriter/readerwriter.c
+++ muict-os-proj1-readerwriter/readerwriter.c
@@ -1,20 +1,61 @@
 #include <stdio.h>
 #include <time.h>
+#include <pthread.h>
+#define SZ 10
+#define RANGE 100
 
-int SZ = 10;
-int ARR[100];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t tid;
+int ARR[RANGE];
 
 void generate()
 {
     // generate random
     srand(time(NULL));
-    for (int i = 0; i < SZ; ++i) {
-        ARR[i] = rand() % 500;
+    for (int i = 0; i < SZ; ++i)
+    {
+        ARR[i] = rand() % RANGE;
     }
 }
 
-int main()
+void *writer(void *num)
 {
+
+}
+
+void *reader(void *num)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    // verify that correct number of arguments were passed in
+    if (argc != 4)
+    {
+        fprintf(stderr, "USAGE:./readme <WRITER THREAD NO.> <READER THREAD NO.> <OUTPUT.TXT>\n");
+        exit(-1);
+    }
+
+    // get reader/writer threads count and output file
+    int numWriter = atoi(argv[1]);
+    int numReader = atoi(argv[2]);
+
     generate();
+
+    // generate reader threads
+    for (int i = 0; i < numReader; ++i)
+    {
+        // using default thread attributes (NULL)
+        pthread_create(&tid, NULL, reader, (void *)numReader);
+    }
+
+    // generate writer threads
+    for (int i = 0; i < numWriter; ++i)
+    {
+        // using default thread attributes (NULL)
+        pthread_create(&tid, NULL, reader, (void *)numWriter);
+    }
+
     return 0;
 }
